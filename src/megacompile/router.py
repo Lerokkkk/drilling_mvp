7a9d065e6561fefb73228a62fc9e639a19348e32
Lerokkkk <vailyinykh@gmail.com>
2024-10-01 16:48:00 +0500

    done megacompiles & add env example

--- src/megacompile/router.py
+++ src/megacompile/router.py
@@ -23,15 +23,9 @@ async def create_mega_compile(machine_id: int, name_id: int, dto: list[CreateMeg
     return [ShowDateTimes(get_time=item) for item in res]
 
 
-@mega_compile_router.get('/1213/machine/{machine_id}/name/{name_id}', response_model=list[ValueTimeMegaCompile])
+@mega_compile_router.get('/machine/{machine_id}/name/{name_id}', response_model=list[MachineNameMegaCompile])
 async def get_mega_compiles(machine_id: int, name_id: int, db: AsyncSession = Depends(get_db)):
-    res = await MegaCompileService(db).get_mega_compile_by_fk(machine_id, name_id)
-    print(res)
-    temp = res[0]
-    print(MachineNameMegaCompile(compile_data=temp[0], machine_name=temp[1], name=temp[2]))
-    return res
-
-
-@mega_compile_router.get('hello')
-async def hello():
-    return "hello"
+    temp_res = await MegaCompileService(db).get_mega_compile_by_fk(machine_id, name_id)
+    response = [MachineNameMegaCompile.from_tuple(res) for res in temp_res]
+    print(response)
+    return response
--- src/megacompile/schemas.py
+++ src/megacompile/schemas.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 from pydantic import BaseModel
+from sqlalchemy.sql.coercions import cls
+from typing_extensions import Any, Self
 
 
 class BaseMegaCompile(BaseModel):
@@ -30,8 +32,23 @@ class ValueTimeMegaCompile(BaseModel):
     value: float
     get_time: datetime
 
+    @classmethod
+    def from_orm(cls, obj: Any) -> Self:
+        return cls(value=obj.value, get_time=obj.get_time)
+
 
 class MachineNameMegaCompile(BaseModel):
     compile_data: ValueTimeMegaCompile
     machine_name: str
     name: str
+
+    @classmethod
+    def from_tuple(cls, obj_tuple):
+        return cls(
+            compile_data=ValueTimeMegaCompile.from_orm(obj_tuple[0]),
+            machine_name=obj_tuple[1],
+            name=obj_tuple[2]
+        )
+
+    class Config:
+        from_attributes = True
